be409177423d628e253ac09894d1224c9f5ecaee
Ian <ianelicha@gmail.com>
2021-09-28 20:02:47 -0600

    Fix styles, keys on table

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -12,10 +12,14 @@ export function UserTable({}: UserTableProps) {
   function TableEntry({ id, firstName, lastName, dateOfBirth }: User) {
     return (
       <tr>
-        <th scope="row">{id}</th>
+        <th scope="row" className="col-sm-1 text-center">
+          {id}
+        </th>
         <td>{firstName}</td>
         <td>{lastName}</td>
-        <td>{dateOfBirth}</td>
+        <td className="col-sm-1">{dateOfBirth}</td>
+        <td className="col-sm-1">Edit</td>
+        <td className="col-sm-1">Delete</td>
       </tr>
     );
   }
@@ -34,18 +38,28 @@ export function UserTable({}: UserTableProps) {
 
   return (
     <div className="table-responsive">
-      <table className="table">
+      <table className="table table-hover table-striped">
         <thead>
           <tr>
-            <th scope="col">#</th>
+            <th scope="col" className="col-sm-1 text-center">
+              #
+            </th>
             <th scope="col">First Name</th>
             <th scope="col">Last Name</th>
-            <th scope="col">DOB</th>
+            <th scope="col" className="col-sm-1">
+              DOB
+            </th>
+            <th scope="col" className="col-sm-1">
+              Edit
+            </th>
+            <th scope="col" className="col-sm-1">
+              Delete
+            </th>
           </tr>
         </thead>
         <tbody>
           {entries.map((user) => (
-            <TableEntry {...user} />
+            <TableEntry key={user.id} {...user} />
           ))}
         </tbody>
       </table>
