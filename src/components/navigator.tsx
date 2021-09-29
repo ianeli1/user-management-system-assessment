5d0be040d43de80e52af429020b835cf69dd07fb
Ian <ianelicha@gmail.com>
2021-09-28 22:55:45 -0600

    Add a link to /add

--- src/components/navigator.tsx
+++ src/components/navigator.tsx
@@ -1,7 +1,19 @@
+import { Link, useHistory } from "react-router-dom";
+import { Button } from "./button";
+
 export function Nav() {
+  const { replace } = useHistory();
+
   return (
-    <nav className="nav">
-      <h1 className="nav-link">User management system</h1>
+    <nav className="nav justify-content-between">
+      <h1
+        className="nav-link"
+        onClick={() => replace("/")}
+        style={{ cursor: "pointer" }}
+      >
+        User management system
+      </h1>
+      <Button onClick={() => replace("/add")}>Add user</Button>
     </nav>
   );
 }
