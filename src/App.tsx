7767fab405de1f5a8040d184b7217e1781f998c5
Ian <ianelicha@gmail.com>
2021-09-28 20:41:08 -0600

    Add router, table to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Nav } from "./components/navigator";
+import { UserTable } from "./components/table";
+import { Route, Switch } from "react-router";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Nav />
+      <div className="container">
+        <Switch>
+          <Route path={["/:page", "/"]}>
+            <UserTable
+              onDelete={(x) => console.log(x)}
+              onEdit={(x) => console.log(x)}
+            />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
